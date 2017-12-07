f51efe8f470d74b06658c47148cbbfd40509c445
Tolga Şen <tolgasen9558@gmail.com>
2017-12-07 01:32:14 -0500

    Added Enemy Prefab, Set it's Navmesh Agent

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform centralTower;
+
+    NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(centralTower.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
